11d4cc69d78de4c1e0c9da99f7df17d52a9fa5ce
naufal afif <naufalafif58@gmail.com>
2021-08-01 05:28:17 +0700

    add timeout on dockercompose-down

--- setup_on_docker_compose/run.sh
+++ setup_on_docker_compose/run.sh
@@ -1,1 +1,1 @@
-docker-compose down 2> /dev/null; docker rmi compose_prometheus 2> /dev/null; docker-compose up -d
+docker-compose down -t 3 2> /dev/null; docker rmi compose_prometheus 2> /dev/null; docker-compose up -d
