8a4170bda51243e514c8b04dd6cabcf0fbb36245
naufal afif <naufalafif58@gmail.com>
2021-08-02 02:40:02 +0700

    add k8s prefix to k8s file

--- setup_mongodb_exporter_on_kubernetes/run.sh
+++ setup_mongodb_exporter_on_kubernetes/run.sh
@@ -1,3 +1,3 @@
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
+kubectl apply -f k8s_deployment.yaml
+kubectl apply -f k8s_service.yaml
 helm install mongodb-exporter prometheus-community/prometheus-mongodb-exporter -f helm_values.yaml
